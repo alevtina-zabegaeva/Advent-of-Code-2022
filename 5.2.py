4039d00276b596f24f5caabe5c9d27f336f5f274
alevtina-zabegaeva <95501508+alevtina-zabegaeva@users.noreply.github.com>
2022-12-05 17:03:44 +0100

    Add files via upload

---
+++ 5.2.py
@@ -0,0 +1,40 @@
+import re
+
+def move(stacks, number, stack1, stack2):
+    stack1 -= 1
+    stack2 -= 1
+    stacks[stack2].extend(stacks[stack1][-number:])
+    stacks[stack1] = stacks[stack1][:-number]
+
+# filename = '5.1test.txt'
+filename = '5.1input.txt'
+
+table = []
+procedures = []
+with open(filename) as f:
+    while True:
+        line = f.readline()[:-1]
+        if not line:
+            break
+        table.append(list(line[1::4]))
+    table.pop()
+
+    while True:
+        line = f.readline().strip()
+        if not line:
+            break
+        procedures.append(tuple(map(int, re.findall('\d+', line))))
+
+table = table[::-1]
+table = [row for row in zip(*table)]
+
+stacks = []
+for stack in table:
+    stacks.append([s for s in stack if s != ' '])
+
+for procedure in procedures:
+    move(stacks, *procedure)
+print(stacks)
+for stack in stacks:
+    print(stack[-1], end='')
+print()
