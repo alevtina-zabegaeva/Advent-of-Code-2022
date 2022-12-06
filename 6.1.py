bcec3a8d2077dce321ad6c628569da2c9e28129e
alevtina-zabegaeva <95501508+alevtina-zabegaeva@users.noreply.github.com>
2022-12-06 10:33:56 +0100

    Add files via upload

---
+++ 6.1.py
@@ -0,0 +1,19 @@
+def main():
+    N = 4
+
+    filename = '6.1input.txt'
+    with open(filename) as f:
+        signal = f.readline()
+
+    # signal = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+    # signal = "nppdvjthqldpwncqszvftbrmjlhg"
+    # signal = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    # signal = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+
+    for i in range(N, len(signal)):
+        if len(set(signal[i - N:i])) == N:
+            break
+    print(i)
+
+if __name__ == '__main__':
+    main()
